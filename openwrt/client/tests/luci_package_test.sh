64f43ccd8477d313fca9d83ac329fbe32775ab17
reTsubasa <reTsubasa@gmail.com>
2026-08-31 17:44:31 +0800

    test: update OpenWrt package revision contract

--- openwrt/client/tests/luci_package_test.sh
+++ openwrt/client/tests/luci_package_test.sh
@@ -449,8 +449,8 @@ fi
 
 assert_contains "$makefile" '^PKG_NAME:=luci-app-candy$'
 assert_contains "$makefile" '^PKG_VERSION:=0\.4\.0$'
-assert_contains "$makefile" '^PKG_RELEASE:=97$'
-assert_contains "$client_makefile" '^PKG_RELEASE:=97$'
+assert_contains "$makefile" '^PKG_RELEASE:=98$'
+assert_contains "$client_makefile" '^PKG_RELEASE:=98$'
 assert_contains "$client_makefile" 'USERID:=candy-sdwan=789:candy-sdwan=789'
 assert_not_contains "$client_makefile" 'adduser -S'
 assert_contains "$client_makefile" 'id -u candy-sdwan'
